38bb137fc65f1575f2fd85beefe8ceacb66ad15d
omal-harsha <omalharshanuwan@gmail.com>
2023-06-12 09:42:08 +0530

    all pages create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { About } from './pages/About';
+import { Contact } from './pages/Contact';
+import { Footer } from './pages/Footer';
+import { Home } from './pages/Home';
+import { Navbar } from './pages/Navbar';
+import { Projects } from './pages/Projects';
+import { Skills } from './pages/Skills';
 
 function App() {
   return (
     <div>
-      omal
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
     </div>
   );
 }
