687e91cb806187bd31fb3af1017b1b387dea5c8f
omal-harsha <omalharshanuwan@gmail.com>
2023-06-15 18:35:40 +0530

    hide key and config vercel

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -28,7 +28,7 @@ export const Contact = () => {
     setBtnText("Sending...")
     
   //email send
-  emailjs.send(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID, userData, 'DhnDLjr1U1wVqzo6s')
+  emailjs.send(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID, userData, process.env.REACT_APP_PUBLIC_KEY)
     .then((response) => {
        console.log('SUCCESS!', response.status, response.text);
        setUserData({
