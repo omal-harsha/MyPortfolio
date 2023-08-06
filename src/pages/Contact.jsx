5edbca812bedeab4e9cf2395e329ec18a06bc36a
Omal Harsha <omalharshanuwan@gmail.com>
2023-08-06 12:14:08 +0530

    contact page animation update

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -59,15 +59,21 @@ export const Contact = () => {
           
           </button>
       </form>
-      <div className='self-end'
+
+    <div>
+      {/* web screen */}
+      <div 
       data-aos="slide-left"
-      
+      data-aos-delay="200"
       >
-        {/* web screen */}
-        <img src='https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/myPhoto%2Fcontact(web).png?alt=media&token=ec6dcda3-1c73-4ad6-9517-990b9a65d11b' className='w-[800px] hidden md:flex'/>
-        {/* mobile screen */}
+        <img src='https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/myPhoto%2Fcontact(web).png?alt=media&token=ec6dcda3-1c73-4ad6-9517-990b9a65d11b' className='w-[800px] hidden md:flex'/>  
+      </div>
+      
+      {/* mobile screen */}
+      <div className='self-end'>  
         <img src='https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/myPhoto%2Fcontact(mobile).png?alt=media&token=9b21f084-179b-4f73-bdda-2be69beaad90' className='md:hidden w-[300px] self-end '/>
       </div>
+    </div>
     </div>
   )
 }
