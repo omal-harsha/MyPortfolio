e4252bffa82ba99a42829947027f09d77490c948
Omal Harsha <69068196+omal-harsha@users.noreply.github.com>
2023-09-30 15:09:18 +0530

    Unlock new skills

--- src/components/SkillsData.js
+++ src/components/SkillsData.js
@@ -8,98 +8,147 @@ export const skillsData = [
     },
     {
         id: 2,
-        logo: "https://img.icons8.com/color/144/react-native.png",
-        title: "React",
-        desc: "(JavaScript library)"
-    },
-    {
-        id: 3,
-        logo: "https://img.icons8.com/color/144/tailwind_css.png",
-        title: "Tailwind css",
-        desc: "(CSS framework)"
-    },
-    {
-        id: 4,
-        logo:"https://img.icons8.com/color/144/figma--v1.png",
-        title: "Figma",
-        desc: "(design and prototyping tool)"
-    },
-    {
-        id: 5,
-        logo:"https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Ficons8-next.js-144.png?alt=media&token=0a89718f-3800-4a08-ab74-6efde3435664",
-        title: "Next.js",
-        desc: "(React framework)"
-    },
-    {
-        id: 6,
-        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Ficons8-nodejs-144.png?alt=media&token=93d42f99-f465-4b3b-a82c-e603774d4161",
-        title: "Node.js",
-        desc: "(JavaScript runtime environment)"
-    },
-    {
-        id: 7,
-        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Ficons8-express-js-144.png?alt=media&token=8a2a9049-a5fa-468a-ad11-a11bab3ab022",
-        title: "Express.js",
-        desc: "( framework for Node.js)"
-    },
-    {
-        id: 8,
-        logo: "https://img.icons8.com/fluency/144/mysql-logo.png",
-        title: "Mysql Workbench",
-        desc: "(database design tool)"
-    },      
-    {
-        id: 9,
-        logo: "https://img.icons8.com/color/144/adobe-photoshop--v1.png",
-        title: "Photoshop",
-        desc: "(image editing software)"
-    },
-    {
-        id: 10,
-        logo: "https://img.icons8.com/color/144/adobe-premiere-pro--v1.png",
-        title: "Adobe Premiere",
-        desc: "(video editing software)               "
-    },
-    {
-        id: 11,
         logo: "https://img.icons8.com/color/144/javascript--v1.png",
         title: "JavaScript",
         desc: "(programming language)"
     },
     {
-        id: 12,
+        id: 3,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Ftypescript.png?alt=media&token=25923009-6e45-4883-acaa-d0e76ecaa60d&_gl=1*n280a2*_ga*ODA5OTMwOTI1LjE2OTYwNjMyMTg.*_ga_CW55HF8NVT*MTY5NjA2MzIxOC4xLjEuMTY5NjA2MzQ1Mi42MC4wLjA.",
+        title: "TypeScript",
+        desc: "(Programming language)"
+    },
+    {
+        id: 4,
+        logo: "https://img.icons8.com/color/144/react-native.png",
+        title: "React",
+        desc: "(JavaScript library)"
+    },
+    {
+        id: 5,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Ficons8-nodejs-144.png?alt=media&token=93d42f99-f465-4b3b-a82c-e603774d4161",
+        title: "Node.js",
+        desc: "(JavaScript runtime environment)"
+    },
+    {
+        id: 6,
+        logo:"https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Ficons8-next.js-144.png?alt=media&token=0a89718f-3800-4a08-ab74-6efde3435664",
+        title: "Next.js",
+        desc: "(React framework)"
+    },
+    {
+        id: 7,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Ficons8-express-js-144.png?alt=media&token=8a2a9049-a5fa-468a-ad11-a11bab3ab022",
+        title: "Express.js",
+        desc: "(framework for Node.js)"
+    },
+    {
+        id: 8,
         logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FecmaScript-144.png?alt=media&token=fc9f7c01-8989-49d5-9d40-12eb828c9b91",
         title: "ECMAScript",
         desc: "(a standardized version)"
     },
     {
+        id: 9,
+        logo: "https://img.icons8.com/fluency/144/mysql-logo.png",
+        title: "MySQL",
+        desc: "(Relational Database Management System)"
+    },
+    {
+        id: 10,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Fpostgresql.png?alt=media&token=f4ad027c-d1b2-4bbd-a640-fd30186af465&_gl=1*3idif4*_ga*ODA5OTMwOTI1LjE2OTYwNjMyMTg.*_ga_CW55HF8NVT*MTY5NjA2MzIxOC4xLjEuMTY5NjA2MzMyNi42MC4wLjA.",
+        title: "PostgreSQL",
+        desc: "(Relational Database Management System)"
+    },
+    {
+        id: 11,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FmongoDB.png?alt=media&token=9f8e5687-e6b8-4b62-9850-c916bd1adef5&_gl=1*15pjvqh*_ga*ODA5OTMwOTI1LjE2OTYwNjMyMTg.*_ga_CW55HF8NVT*MTY5NjA2MzIxOC4xLjEuMTY5NjA2MzM2Ny4xOS4wLjA.",
+        title: "MongoDB",
+        desc: "(NoSQL Database)"
+    },
+    {
+        id: 12,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Fmysqlworkbench.png?alt=media&token=8714ddd3-cf19-4b9d-9b7a-db779ccb037b&_gl=1*gpruqc*_ga*ODA5OTMwOTI1LjE2OTYwNjMyMTg.*_ga_CW55HF8NVT*MTY5NjA2MzIxOC4xLjEuMTY5NjA2MzY2NC41MS4wLjA.",
+        title: "Mysql Workbench",
+        desc: "(database design tool)"
+    },
+    {
         id: 13,
-        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FVsCode-144.png?alt=media&token=f125daab-c077-4ee0-91bb-a05e51522c05",
-        title: "Visual Studio Code ",
-        desc: "(popular code editor)"
-    },
-    {
-        id: 14,
-        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FpostMan-144.png?alt=media&token=b3a2538e-2c5f-493f-bae9-5924f6a75f6f",
-        title: "Postman ",
-        desc: "(API testing tool)"
-    },
-    {
-        id: 15,
-        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FGitHub-144.png?alt=media&token=88f3e512-c4b5-4b5e-968d-509a0c6c8e5e",
-        title: "GitHub ",
-        desc: "(version control)"
-    },
-    {
-        id: 16,
         logo: "https://img.icons8.com/color/144/firebase.png",
         title: "Firebase",
         desc: "(Cloud app platform)"
     },
     {
+        id: 14,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FSequelize.png?alt=media&token=a2b14c24-7485-4855-9694-77f7c613a204&_gl=1*1i7fkhk*_ga*ODA5OTMwOTI1LjE2OTYwNjMyMTg.*_ga_CW55HF8NVT*MTY5NjA2MzIxOC4xLjEuMTY5NjA2MzM4OS42MC4wLjA.",
+        title: "Sequelize",
+        desc: "(ORMfor Node.js)"
+    },
+    {
+        id: 15,
+        logo: "https://img.icons8.com/color/144/tailwind_css.png",
+        title: "Tailwind css",
+        desc: "(CSS framework)"
+    },
+    {
+        id: 16,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FantD.png?alt=media&token=dfe15eae-1eb5-4bb3-9b9a-0a6d9b26c353&_gl=1*sucqee*_ga*ODA5OTMwOTI1LjE2OTYwNjMyMTg.*_ga_CW55HF8NVT*MTY5NjA2MzIxOC4xLjEuMTY5NjA2MzUyMy42MC4wLjA.",
+        title: "Ant Design",
+        desc: "(Design System and UI Framework)"
+    },
+    {
         id: 17,
+        logo:"https://img.icons8.com/color/144/figma--v1.png",
+        title: "Figma",
+        desc: "(design and prototyping tool)"
+    },
+    {
+        id: 18,
+        logo: "https://img.icons8.com/color/144/adobe-photoshop--v1.png",
+        title: "Photoshop",
+        desc: "(image editing software)"
+    },
+    {
+        id: 19,
+        logo: "https://img.icons8.com/color/144/adobe-premiere-pro--v1.png",
+        title: "Adobe Premiere",
+        desc: "(video editing software)               "
+    },
+    {
+        id: 20,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FVsCode-144.png?alt=media&token=f125daab-c077-4ee0-91bb-a05e51522c05",
+        title: "Visual Studio Code ",
+        desc: "(popular code editor)"
+    },
+    {
+        id: 21,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FpostMan-144.png?alt=media&token=b3a2538e-2c5f-493f-bae9-5924f6a75f6f",
+        title: "Postman ",
+        desc: "(API testing tool)"
+    },
+    {
+        id: 22,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2FGitHub-144.png?alt=media&token=88f3e512-c4b5-4b5e-968d-509a0c6c8e5e",
+        title: "GitHub ",
+        desc: "(version control)"
+    },
+    {
+        id: 23,
         logo: "https://img.icons8.com/color/144/jira.png",
         title: "JIRA",
         desc: "(Project management tool)"
+    },
+    {
+        id: 24,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Fmermaid.png?alt=media&token=63c78dac-cf09-4142-a822-5f7be269e4f6&_gl=1*1t5c94p*_ga*ODA5OTMwOTI1LjE2OTYwNjMyMTg.*_ga_CW55HF8NVT*MTY5NjA2MzIxOC4xLjEuMTY5NjA2MzQ2OS40My4wLjA.",
+        title: "Mermaid",
+        desc: "(Diagram and Flowchart Generator)"
+    },
+    {
+        id: 25,
+        logo: "https://firebasestorage.googleapis.com/v0/b/blogsite-33dcd.appspot.com/o/icons%2Fredux.png?alt=media&token=e5c3cdb9-f77b-4bed-bb57-0851a2120794&_gl=1*akxfya*_ga*ODA5OTMwOTI1LjE2OTYwNjMyMTg.*_ga_CW55HF8NVT*MTY5NjA2NjYyNy4yLjEuMTY5NjA2NjYzMi41NS4wLjA.",
+        title: "Redux",
+        desc: "(State management library)"
     }
+
 ]
